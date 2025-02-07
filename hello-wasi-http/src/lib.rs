6080222c6fd8dd05cac4805c3f567f75d425f576
reo0306 <reo19840306@gmail.com>
2025-02-07 23:19:32 +0900

    add hello-wasi-http

---
+++ hello-wasi-http/src/lib.rs
@@ -0,0 +1,38 @@
+use bindings::exports::wasi::http::incoming_handler::{Guest, IncomingRequest, ResponseOutparam};
+use bindings::wasi::http::types::{Headers, OutgoingResponse, FieldKey, FieldValue, OutgoingBody};
+
+#[allow(warnings)]
+mod bindings;
+
+
+struct Component;
+
+impl Guest for Component {
+    fn handle(_: IncomingRequest, response_out: ResponseOutparam) {
+        let headers = [
+            (
+                FieldKey::from("Content-Type"),
+                FieldValue::from("text/html"),
+            )
+        ];
+
+        let headers = Headers::from_list(&headers).unwrap();
+
+        let response = OutgoingResponse::new(headers);
+
+        response.set_status_code(200).unwrap();
+
+        let body = response.body().unwrap();
+
+        let stream = body.write().unwrap();
+        stream.blocking_write_and_flush(b"Hello, world!").unwrap();
+
+        drop(stream);
+        OutgoingBody::finish(body, None).unwrap();
+
+        ResponseOutparam::set(response_out, Ok(response));
+
+    }
+}
+
+bindings::export!(Component with_types_in bindings);
